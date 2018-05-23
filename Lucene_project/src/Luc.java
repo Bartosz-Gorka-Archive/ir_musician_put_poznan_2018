5b4b67d15a00b2258d65e3545e59bbb95e26a73e
Mateusz <mateusz0831@gmail.com>
2018-05-23 23:15:07 +0200

    Obtain pages from wiki and create simple searcher (#1)
    * Obtain pages from wiki and create simple searcher
    
    - Crawl wikipedia by solr (nutch) -> 524 pages from various category (see TODO.md)
    - Upload important file from solr/nutch instance
    - Create Java project with Lucene (README to install proper library)
    - Load index from nutch and implement simple searcher
    
    * Add new pages to index
    
    - add pages to index, now is 1050
    - attach log file from crawl (was 3 failed status)

---
+++ Lucene_project/src/Luc.java
@@ -0,0 +1,53 @@
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+
+public class Luc {
+    public static void main(String[] args) throws IOException, ParseException {
+
+        // Read index from nutch
+        Directory index = FSDirectory.open(Paths.get("./data/index"));
+        IndexReader reader = DirectoryReader.open(index);
+
+        // Check how many docs are in index
+        int index_size = reader.numDocs();
+        System.out.println("In index are "+index_size+" docs. (In solr are 1050, should be the same!)\n");
+
+
+        // Create searcher
+        IndexSearcher searcher = new IndexSearcher(reader);
+
+        // Prepare simple query string
+        String querystr = "trip-hop";
+
+        StandardAnalyzer analyzer = new StandardAnalyzer();
+        Query q = new QueryParser("content", analyzer).parse(querystr);
+
+        // Config number of output
+        int hitsPerPage = 5;
+
+        // Search and obtain results
+        TopDocs docs = searcher.search(q, hitsPerPage);
+        ScoreDoc[] hits = docs.scoreDocs;
+
+        System.out.println("Found " + hits.length + " hits.");
+        for (int i = 0; i < hits.length; ++i) {
+            int docId = hits[i].doc;
+            Document d = searcher.doc(docId);
+            System.out.println((i + 1) + ". " + hits[i].score + "\t" + d.get("title") + "\t" + d.get("url"));
+        }
+
+    }
+}
