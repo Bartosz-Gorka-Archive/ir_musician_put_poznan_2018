c57995c6b1c2c289a0e3ba908d67cae4e76f26c8
jaroslaw <jarek.skrzypczak@o2.pl>
2018-06-15 18:05:01 +0200

    Rename Luc to Main Optimize imports in Main class

--- Lucene_project/src/Main.java
+++ Lucene_project/src/Main.java
@@ -1,30 +1,20 @@
 import javafx.util.Pair;
-import opennlp.tools.namefind.NameFinderME;
-import opennlp.tools.namefind.TokenNameFinderModel;
-import opennlp.tools.tokenize.TokenizerME;
-import opennlp.tools.tokenize.TokenizerModel;
-import opennlp.tools.util.Span;
-import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.index.IndexableField;
 import org.apache.lucene.queryparser.classic.ParseException;
-import org.apache.lucene.queryparser.classic.QueryParser;
-import org.apache.lucene.search.IndexSearcher;
-import org.apache.lucene.search.Query;
-import org.apache.lucene.search.ScoreDoc;
-import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 
-import java.io.*;
-import java.net.URLDecoder;
+import java.io.File;
+import java.io.IOException;
 import java.nio.file.Paths;
-import java.util.*;
-import java.util.regex.Pattern;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
-public class Luc {
+public class Main {
 
     public static void main(String[] args) throws IOException, ParseException {
 
